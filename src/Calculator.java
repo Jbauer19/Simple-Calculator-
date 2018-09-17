8bc6e07a20905d9c8586967b7449c4b303543806
Jbauer19 <42978498+Jbauer19@users.noreply.github.com>
2018-09-17 17:29:42 -0400

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator 
+{
+	public int add(int num1, int num2)
+	{
+		int ans = (int) (num1 + num2);
+		return ans;
+	}
+	
+	public int sub(int num1, int num2)
+	{
+		int ans = (int) (num1 - num2);
+		return ans;
+	}
+
+}
---
+++ src/CalculatorMain.java
@@ -0,0 +1,24 @@
+
+public class CalculatorMain 
+{
+
+	public static void main(String[] args) 
+	{
+		Calculator calc = new Calculator();
+		
+		int num = calc.add(5, 5);
+		System.out.println(num);
+		
+		int num2 = calc.sub(10,6);
+		System.out.println(num2);
+		
+		
+		
+		
+	
+		
+		
+
+	}
+
+}
